099f91e8018fcb33f3f553a8cb5fd368e188b734
Altamish Mahomed <gambit318@gmail.com>
2020-08-03 17:15:31 +0200

    Move to Pi env

--- python/luna_svr.py
+++ python/luna_svr.py
@@ -1,11 +1,15 @@
-import socket
-import struct
-import threading
+import socket 
+import struct 
 
-import queue
+import threading 
+import queue 
+
+import board
+import neopixel
+
 
 # LED strip configuration:
-LED_COUNT      = 16      # Number of LED pixels.
+LED_COUNT      = 60      # Number of LED pixels.
 LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
 #LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
@@ -13,75 +17,71 @@ LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
 LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+ORDER = neopixel.GRB
+BOARD_PIN = board.D18
 
 
-#HOST = "192.168.8.120"
-HOST = "127.0.0.1"
-
-PORT = 8886
-
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.bind((HOST, PORT))
-print("Listening on UDP %s:%s" % (HOST, PORT))
-
-q = queue.Queue(LED_COUNT)
-
-def processQueue():
-    while True:        
-        if q.empty() == False:
-            item = q.get()
-            #print("Pushed to strip : " + item + "\n")
-
-qprocessor = threading.Thread(target=processQueue)
-qprocessor.start()
-
-# Create NeoPixel object with appropriate configuration.
-#strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
-# Intialize the library (must be called once before other functions).
-#strip.begin()
+pixels = neopixel.NeoPixel(BOARD_PIN, LED_COUNT, brightness=LED_BRIGHTNESS, auto_write=False, pixel_order=ORDER)
 
 
-while True:
-    #(data, addr) = s.recvfrom(4)
-    (data, addr) = s.recvfrom(8)#64bit Windows hack
-    #print(str(data))
-   
-    num = int.from_bytes(data, 'little')
-    #print(num)
-   
-    sb = str("{0:b}".format(num)).zfill(32)#DO WE EVEN NEED TO ZFILL HERE?!  
-    sb = sb[32:64]#64bit Windows hack
-    #print(sb)
-   
-    ib = sb[0:8]
-    i = int(ib, 2)
-    #print("i : " + ib + " = " + str(i))
+HOST = "192.168.8.154" 
+#HOST = "127.0.0.1" 
+ 
+PORT = 8886 
+ 
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+s.bind((HOST, PORT)) 
+print("Listening on UDP %s:%s" % (HOST, PORT)) 
+
+q = queue.Queue(LED_COUNT) 
+ 
+def processQueue(): 
+    while True:         
+        if q.empty() == False: 
+            item = q.get() 
+            print("Pushed to strip : " + str(item[0]) +  "|" + str(item[1][0]) + "|" + str(item[1][1]) +  "|" + str(item[1][2]) + "\n") 
+ 
+qprocessor = threading.Thread(target=processQueue) 
+qprocessor.start() 
+
+while True: 
+    (data, addr) = s.recvfrom(4) 
+    #print(str(data)) 
     
-    rb = sb[8:16]
-    r = int(rb, 2)
-    #print("r : " + rb + " = " + str(r))
+    num = int.from_bytes(data, 'little') 
+    #print(num) 
     
-    gb = sb[16:24]
-    g = int(gb, 2)
-    #print("g : " + gb + " = " + str(g))
+    sb = str("{0:b}".format(num)).zfill(32)     
+    print(sb) 
     
-    bb= sb[24:32]
-    b = int(bb, 2)
-    #print("b : " + gb + " = " + str(b))
-   
-    #print(str(i) + " | " + str(r)  + " | " + str(g) + " | " + str(b))
+    ib = sb[0:8] 
+    i = int(ib, 2) 
+    print("i : " + ib + " = " + str(i)) 
+    
+    rb = sb[8:16] 
+    r = int(rb, 2) 
+    print("r : " + rb + " = " + str(r)) 
+    
+    gb = sb[16:24] 
+    g = int(gb, 2) 
+    print("g : " + gb + " = " + str(g)) 
+    
+    bb= sb[24:32] 
+    b = int(bb, 2) 
+    print("b : " + gb + " = " + str(b)) 
+    
+    #print(str(i) + " | " + str(r)  + " | "  + " | " + str(g) + " | " + str(b)) 
     #print(str(i))
     
-    #strip.setPixelColor(i, sb)
-    #strip.show()
-
     if q.full() == True:
-        #print("FULLLLL!!!!!!!\n")
-        q.get()
-        #print("Removed from q : " + item)
-            
-    q.put(sb)
-    #print("Added to q : " + sb)
-    
-    #print()
+        #print("FULLLLL!!!!!!!\n") 
+        q.get() 
+        #print("Removed from q : " + item) 
+             
+    item = [i,(r,g,b)]
+    q.put(item)
+    #print("Added to q : " + item) 
+     
+    print() 
+
