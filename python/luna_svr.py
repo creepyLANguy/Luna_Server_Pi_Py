ff057f60e71ad37ad1eda93c23707f008141d4f9
Altamish Mahomed <gambit318@gmail.com>
2020-10-18 20:47:20 +0200

    Improved handling of reserved packets.

--- python/luna_svr.py
+++ python/luna_svr.py
@@ -9,24 +9,24 @@ import neopixel
 
 import time
 
+MAX_INDEX = 249
+FILL_INDEX = 255
+
 # LED strip configuration:
 LED_COUNT      = 28     # Number of LED pixels.
-LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
-#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_BRIGHTNESS = 0.5     # [0.0, 1.0]
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
-LED_BRIGHTNESS = 0.5     # [0.0, 1.0]
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 ORDER = neopixel.GRB
 BOARD_PIN = board.D18
 AUTO_WRITE = False
 
-pixels = neopixel.NeoPixel(BOARD_PIN, LED_COUNT,  brightness=LED_BRIGHTNESS, auto_write=AUTO_WRITE, pixel_order=ORDER)
+pixels = neopixel.NeoPixel(BOARD_PIN, LED_COUNT, brightness=LED_BRIGHTNESS, auto_write=AUTO_WRITE, pixel_order=ORDER)
+
 
 HOST = "192.168.0.174"
-#HOST = "127.0.0.1"
-
 PORT = 8886
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -34,27 +34,22 @@ s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind((HOST, PORT))
 print("Listening on UDP %s:%s" % (HOST, PORT))
 
+
 q = queue.Queue(LED_COUNT)
 
 def processQueue():
     runs = 0
     while True:        
         if q.empty() == False:
-            item = q.get()
-            if item[0] == 255:
-                for x in range(LED_COUNT):
-                    pixels[x] = (item[1],item[2],item[3])
-                pixels.show()
-                print("-->\tOUT\t" + "[ALL]" + "\t|" + str(item[1]) + "\t|" + str(item[2]) + "\t|" + str(item[3]))
-            else:                
-                pixels[item[0]] = (item[1],item[2],item[3])
-    #            runs = runs + 1
-    #            if runs == LED_COUNT:            
-    #                pixels.show()
-    #                runs = 0
-    #                print("STRIP UPDATED!")
-                pixels.show()
-                print("-->\tOUT\t" + str(item[0]) + "\t|" + str(item[1]) + "\t|" + str(item[2]) + "\t|" + str(item[3]))
+            item = q.get()                
+            pixels[item[0]] = (item[1],item[2],item[3])
+#            runs = runs + 1
+#            if runs == LED_COUNT:            
+#                pixels.show()
+#                runs = 0
+#                print("STRIP UPDATED!")
+            pixels.show()
+            print("-->\tOUT\t" + str(item[0]) + "\t|" + str(item[1]) + "\t|" + str(item[2]) + "\t|" + str(item[3]))
 
 qprocessor = threading.Thread(target=processQueue)
 qprocessor.start()
@@ -94,10 +89,6 @@ while True:
     #print("b : " + gb + " = " + str(b))
    
     #print(str(i) + " | " + str(r)  + " | " + str(g) + " | " + str(b))
-    #print(str(i))
-    
-    #strip.setPixelColor(i, sb)
-    #strip.show()
 
     if q.full() == True:
         #print("FULLLLL!!!!!!!\n")
@@ -105,11 +96,15 @@ while True:
         #print("Removed from q : " + removedItem[0]) 
         q.get()        
             
-    q.put((i,r,g,b))
-    #print("Added to q : " + sb)
-    if i == 255:
-        print("<--\tIN\t" + "[ALL]" + "\t|" + str(r)  + "\t|" + str(g) + "\t|" + str(b))
+    if i > MAX_INDEX:        
+        if i == FILL_INDEX:
+            pixels.fill((r,g,b))
+            pixels.show()
+            print("<-->\t[ALL]\t" + str(r) + "\t|" + str(g) + "\t|" + str(b))
+            q.queue.clear()
+        else:
+            print("UNIMPLEMENTED PACKET TYPE")
     else:
+        q.put((i,r,g,b))
         print("<--\tIN\t" + str(i) + "\t|" + str(r)  + "\t|" + str(g) + "\t|" + str(b))
-    #print()
 
